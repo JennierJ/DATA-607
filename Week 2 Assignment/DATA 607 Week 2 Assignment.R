3a19705f2634a3d471104b647581abf69edf11bc
Jenny <jennyxie2015@gmail.com>
2017-02-12 22:39:06 -0500

    DATA607Week 2

---
+++ Week 2 Assignment/DATA 607 Week 2 Assignment.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+movie_reviews <- read.csv("https://raw.githubusercontent.com/Jennier2015/DATA-607/master/Week%202%20Assignment/movie_review.csv")
+movie_reviews <- as.data.frame(movie_reviews)
+movie_reviews <- as.data.frame(movie_reviews)
+is.data.frame(movie_reviews)
+head(movie_reviews)
+
